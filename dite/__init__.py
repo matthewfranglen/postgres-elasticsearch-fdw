58ed66631a87c1e75a9e10bbfc0176a52833d26e
Matthew Franglen <matthew@franglen.org>
2015-12-01 11:47:09 +0000

    Attempt to address pylint failures
     * 'id' is a builtin and should not be used as a variable name
     * The docstrings for many methods and for the package were missing
     * The comparisons were non standard (things like `not 200 == resp.code`)
    
    Some of the warnings from pylint were ignored - apparently asteroid has
    a problem determining if a class is 'old style'. Also there are
    currently unused arguments which cannot be usefully used.

--- dite/__init__.py
+++ dite/__init__.py
@@ -1,9 +1,7 @@
-###
-### Author: Mikulas Dite
-### Time-stamp: <2015-06-09 21:54:14 dwa>
-
-from multicorn import ForeignDataWrapper
-from multicorn.utils import log_to_postgres as log2pg
+""" Elastic Search foreign data wrapper
+    Author: Mikulas Dite
+"""
+# pylint: disable=super-on-old-class, unused-argument
 
 from functools import partial
 
@@ -11,7 +9,12 @@ import httplib
 import json
 import logging
 
-class ElasticsearchFDW (ForeignDataWrapper):
+from multicorn import ForeignDataWrapper
+from multicorn.utils import log_to_postgres as log2pg
+
+
+class ElasticsearchFDW(ForeignDataWrapper):
+    """ Elastic Search Foreign Data Wrapper """
 
     def __init__(self, options, columns):
         super(ElasticsearchFDW, self).__init__(options, columns)
@@ -24,27 +27,29 @@ class ElasticsearchFDW (ForeignDataWrapper):
         self.columns = columns
 
     def get_rel_size(self, quals, columns):
-        """Helps the planner by returning costs.
-
-        Returns a tuple of the form (nb_row, avg width)
-        """
+        """ Helps the planner by returning costs.
+            Returns a tuple of the form (nb_row, avg width) """
 
         conn = httplib.HTTPConnection(self.host, self.port)
         conn.request("GET", "/%s/%s/_count" % (self.node, self.index))
         resp = conn.getresponse()
-        if not 200 == resp.status:
+
+        if resp.status != 200:
             return (0, 0)
 
         raw = resp.read()
         data = json.loads(raw)
-        # log2pg('MARK RESPONSE: >>%d<<' % data['count'], logging.DEBUG)
+
         return (data['count'], len(columns) * 100)
 
     def execute(self, quals, columns):
+        """ Execute the query """
+
         conn = httplib.HTTPConnection(self.host, self.port)
         conn.request("GET", "/%s/%s/_search?q=*:*&size=100000000" % (self.node, self.index))
         resp = conn.getresponse()
-        if not 200 == resp.status:
+
+        if resp.status != 200:
             return (0, 0)
 
         raw = resp.read()
@@ -63,54 +68,57 @@ class ElasticsearchFDW (ForeignDataWrapper):
 
     @property
     def rowid_column(self):
-        """Returns a column name which will act as a rowid column,
-        for delete/update operations. This can be either an existing column
-        name, or a made-up one.
-        This column name should be subsequently present in every
-        returned resultset.
-        """
-        return 'id';
+        """ Returns a column name which will act as a rowid column for
+            delete/update operations.
 
-    def es_index(self, id, values):
-        content = json.dumps(values)
+            This can be either an existing column name, or a made-up one. This
+            column name should be subsequently present in every returned
+            resultset. """
 
-        conn = httplib.HTTPConnection(self.host, self.port)
-        conn.request("PUT", "/%s/%s/%s" % (self.node, self.index, id), content)
-        resp = conn.getresponse()
-        if not 200 == resp.status:
-            return
-
-        raw = resp.read()
-        data = json.loads(raw)
-
-        return data
+        return 'id'
 
     def insert(self, new_values):
+        """ Insert new documents into Elastic Search """
         log2pg('MARK Insert Request - new values:  %s' % new_values, logging.DEBUG)
 
-        if not 'id' in new_values:
-             log2pg('INSERT requires "id" column.  Missing in: %s' % new_values, logging.ERROR)
+        if 'id' not in new_values:
+            log2pg('INSERT requires "id" column.  Missing in: %s' % new_values, logging.ERROR)
 
-        id = new_values['id']
+        document_id = new_values['id']
         new_values.pop('id', None)
-        return self.es_index(id, new_values)
+        return self._upsert(document_id, new_values)
 
-    def update(self, id, new_values):
+    def update(self, document_id, new_values):
+        """ Update existing documents in Elastic Search """
+
         new_values.pop('id', None)
-        return self.es_index(id, new_values)
+        return self._upsert(document_id, new_values)
 
-    def delete(self, id):
+    def delete(self, document_id):
+        """ Delete documents from Elastic Search """
+
         conn = httplib.HTTPConnection(self.host, self.port)
-        conn.request("DELETE", "/%s/%s/%s" % (self.node, self.index, id))
+        conn.request("DELETE", "/%s/%s/%s" % (self.node, self.index, document_id))
         resp = conn.getresponse()
-        if not 200 == resp.status:
+        if resp.status != 200:
             log2pg('Failed to delete: %s' % resp.read(), logging.ERROR)
             return
 
         raw = resp.read()
         return json.loads(raw)
 
-## Local Variables: ***
-## mode:python ***
-## coding: utf-8 ***
-## End: ***
+    def _upsert(self, document_id, values):
+        """ Insert or Update the document in Elastic Search """
+        content = json.dumps(values)
+
+        conn = httplib.HTTPConnection(self.host, self.port)
+        conn.request("PUT", "/%s/%s/%s" % (self.node, self.index, document_id), content)
+        resp = conn.getresponse()
+
+        if resp.status != 200:
+            return
+
+        raw = resp.read()
+        data = json.loads(raw)
+
+        return data
