466c263aff1a94586c404922060f4f0dadd6f5fc
Matthew Franglen <matthew@franglen.org>
2015-12-01 21:22:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
-###
-### Author:
-### Time-stamp: <2010-03-04 23:04:21 dwa>
-
+""" Install file for Postgres Elasticsearch Foreign Data Wrapper """
 from setuptools import setup
 
 if __name__ == '__main__':
     setup(
-	  name='dite',
-	  version='0.0.1',
-	  author='Mikulas Dite',
-	  license='Postgresql',
-	  packages=['dite']
-	)
-## Local Variables: ***
-## mode:python ***
-## coding: utf-8 ***
-## End: ***
+        name='pg_es',
+        version='0.0.1',
+        license='Postgresql',
+        packages=['pg_es']
+    )
