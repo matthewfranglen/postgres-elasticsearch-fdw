cf4bc623182e3f80677404f096b7ce49a057b9ac
Matthew Franglen <matthew@franglen.org>
2015-12-01 10:31:46 +0000

    Add simple sql example
    This doesn't update the data and it also doesn't use triggers off the
    original table to do the work. Need to implement bulk inserts and it's
    unlikely that triggers will be suitable for that.

---
+++ sql/example.sql
@@ -0,0 +1,51 @@
+CREATE EXTENSION multicorn;
+
+CREATE SERVER multicorn_es FOREIGN DATA WRAPPER multicorn
+OPTIONS (
+  wrapper 'dite.ElasticsearchFDW'
+);
+
+CREATE FOREIGN TABLE articles_es
+    (
+        id BIGINT,
+        title TEXT,
+        content TEXT
+    )
+SERVER multicorn_es
+OPTIONS
+    (
+        host 'elasticsearch',
+        port '9200',
+        node 'test',
+        index 'articles'
+    )
+;
+
+SELECT
+    id,
+    title,
+    content
+FROM
+    articles_es
+;
+
+INSERT INTO articles_es
+    (
+        id,
+        title,
+        content
+    )
+VALUES
+    (
+        1,
+        'foo',
+        'spike'
+    );
+
+SELECT
+    id,
+    title,
+    content
+FROM
+    articles_es
+;
