66e4c066bdb1126e52b70c57b63ee0c6a2176961
Matthew Franglen <matthew@franglen.org>
2017-04-11 23:28:35 +0100

    Making test directory

---
+++ tests/sql/schema.sql
@@ -0,0 +1,35 @@
+CREATE EXTENSION multicorn;
+
+CREATE SERVER multicorn_es FOREIGN DATA WRAPPER multicorn
+OPTIONS (
+  wrapper 'pg_es.ElasticsearchFDW'
+);
+
+CREATE TABLE articles
+    (
+        id BIGINT,
+        title TEXT,
+        content TEXT
+    )
+;
+
+CREATE FOREIGN TABLE articles_es
+    (
+        id BIGINT,
+        title TEXT,
+        content TEXT,
+        query TEXT,
+        score NUMERIC
+    )
+SERVER multicorn_es
+OPTIONS
+    (
+        host 'elasticsearch',
+        port '9200',
+        index 'article-index',
+        type 'article',
+        rowid_column 'id',
+        query_column 'query',
+        score_column 'score'
+    )
+;
