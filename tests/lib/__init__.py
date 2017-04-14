3c24b03b936f4901e04ce5f328bff508a8acdc43
Matthew Franglen <matthew@franglen.org>
2017-04-14 11:51:21 +0100

    Working on lib

--- tests/lib/__init__.py
+++ tests/lib/__init__.py
@@ -1,6 +1,8 @@
 from os.path import abspath, dirname, join
 
+import io
 import psycopg2
+import re
 import requests
 import sh
 import time
@@ -9,9 +11,15 @@ PROJECT_FOLDER=dirname(dirname(dirname(abspath(__file__))))
 TEST_FOLDER=join(PROJECT_FOLDER, 'tests')
 DOCKER_FOLDER=join(TEST_FOLDER, 'docker')
 
-def docker_compose(version, *args):
+def docker_compose(version):
     compose_file=join(DOCKER_FOLDER, version, 'docker-compose.yml')
-    sh.docker_compose('-f', compose_file, *args)
+    return lambda *args, **kwargs: sh.docker_compose('-f', compose_file, *args, **kwargs)
+
+def exec_container(version, container):
+    with io.StringIO() as buf:
+        docker_compose(version)('ps', '-q', container, _out=buf)
+        container=re.sub(r'\n.*', '', buf.getvalue())
+        return lambda *args: sh.docker('exec', '-i', container, *args)
 
 def wait_for(condition):
     for i in xrange(120):
@@ -30,6 +38,10 @@ def test_es():
         return requests.get('http://localhost:9200').json()['tagline'] == 'You Know, for Search'
     except Exception:
         return False
+
+def load_sql_file(version, filename):
+    with open(filename, 'r') as handle:
+        exec_container(version, 'postgres')('psql' '--username' 'postgres' 'postgres', _in=handle)
 
 def sql(statement):
     with psycopg2.connect(host='localhost', port=5432, user='postgres', dbname='postgres') as conn:
