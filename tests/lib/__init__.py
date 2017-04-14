6c417f49ebd0f466ebf8100a28006e7c5ea2ff3d
Matthew Franglen <matthew@franglen.org>
2017-04-14 11:00:57 +0100

    Create somewhat questionable sql function

--- tests/lib/__init__.py
+++ tests/lib/__init__.py
@@ -11,16 +11,24 @@ def docker_compose(version, *args):
     sh.docker_compose('-f', 'docker/{version}/docker-compose.yml'.format(version=version), *args)
 
 def test_pg():
-    try:
-        with psycopg2.connect(host='localhost', port=5432, user='postgres', dbname='postgres') as conn:
-            with conn.cursor() as cursor:
-                cursor.execute('select 1 + 1;')
-                return cursor.fetchone()[0] == 2
-    except Exception:
+    def callback(cursor):
+        cursor.execute('select 1 + 1;')
+        return cursor.fetchone()[0] == 2
+    def error():
         return False
+
+    return sql(callback, error)
 
 def test_es():
     try:
         return requests.get('http://localhost:9200').json()['tagline'] == 'You Know, for Search'
     except Exception:
         return False
+
+def sql(callback, error):
+    try:
+        with psycopg2.connect(host='localhost', port=5432, user='postgres', dbname='postgres') as conn:
+            with conn.cursor() as cursor:
+                return callback(cursor)
+    except Exception:
+        return error()
