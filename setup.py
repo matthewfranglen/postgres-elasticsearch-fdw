c8b9267ad1ae63aae41660f69cc3d7dde3299cdc
Matthew Franglen <matthew@franglen.org>
2017-07-28 22:14:50 +0100

    Increase version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ if __name__ == '__main__':
     setup(
         name = 'pg_es_fdw',
         packages = ['pg_es_fdw'],
-        version = '0.4.1',
+        version = '0.4.2',
         description = 'Connect PostgreSQL and Elastic Search with this Foreign Data Wrapper',
         author = 'Matthew Franglen',
         author_email = 'matthew@franglen.org',
         url = 'https://github.com/matthewfranglen/postgres-elasticsearch-fdw',
-        download_url = 'https://github.com/matthewfranglen/postgres-elasticsearch-fdw/archive/0.4.1.zip',
+        download_url = 'https://github.com/matthewfranglen/postgres-elasticsearch-fdw/archive/0.4.2.zip',
         keywords = ['postgres', 'postgresql', 'elastic', 'elastic search', 'fdw'],
         install_requires=[
             'elasticsearch',
