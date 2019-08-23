a8837ffe5f2a5597010d86bca4aca2f6948b765d
Matthew Franglen <matthew@franglen.org>
2019-08-23 21:56:15 +0100

    Fix tools that were not updated for varying es versions

--- tests/lib/docker_tools.py
+++ tests/lib/docker_tools.py
@@ -34,25 +34,25 @@ def docker_compose(pg_version, es_version):
     )
 
 
-def exec_container(version, container):
+def exec_container(pg_version, es_version, container):
     """ Wrapper around executing commands in a container """
 
-    container = container_name(version, container)
+    container = container_name(pg_version, es_version, container)
     return sh.docker.bake("exec", "-i", container)
 
 
-def container_name(version, container):
+def container_name(pg_version, es_version, container):
     """ Determine container name """
 
     with io.StringIO() as buf:
-        docker_compose(version)("ps", "-q", container, _out=buf)
+        docker_compose(pg_version, es_version)("ps", "-q", container, _out=buf)
         return re.sub(r"\n.*", "", buf.getvalue())
 
 
-def container_port(version, container, port):
+def container_port(pg_version, es_version, container, port):
     """ Determine mapped container port """
 
     with io.StringIO() as buf:
-        docker_compose(version)("port", container, port, _out=buf)
+        docker_compose(pg_version, es_version)("port", container, port, _out=buf)
         host_and_port = re.sub(r"\n.*", "", buf.getvalue())
         return re.sub(r".*:", "", host_and_port)
--- tests/start.py
+++ tests/start.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# pylint: disable=line-too-long
 """ Start the containers """
 
 import argparse
@@ -10,12 +11,18 @@ def main():
     """ Start the containers """
 
     parser = argparse.ArgumentParser(description="Set up testing environment.")
-    parser.add_argument("version", help="PostgreSQL version")
+    parser.add_argument("--pg", help="PostgreSQL version")
+    parser.add_argument("--es", help="Elasticsearch version")
     args = parser.parse_args()
 
-    version = args.version
-    print("Starting environment with PostgreSQL {version}".format(version=version))
-    set_up(version)
+    pg_version = args.pg
+    es_version = args.es
+    print(
+        "Starting environment with PostgreSQL {pg_version} with Elasticsearch {es_version}...".format(
+            pg_version=pg_version, es_version=es_version
+        )
+    )
+    set_up(pg_version, es_version)
 
 
 if __name__ == "__main__":
