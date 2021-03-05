cae41ee8500ee1bf453bfc1e58a3c74dadd4955e
Matthew Franglen <matthew@franglen.org>
2021-03-05 07:56:43 +0000

    Use the text from the ticket

--- tests/test/insert-chinese.sql
+++ tests/test/insert-chinese.sql
@@ -1,2 +1,2 @@
 INSERT INTO articles_es (id, title, body)
-VALUES (1, 'simplified Chinese 汉语', 'traditional Chinese 漢語') returning id;
+VALUES (1, 'simplified Chinese 汉语 北京市丰台区', 'traditional Chinese 漢語 测试字段测试字段测试字段测试字段') returning id;
