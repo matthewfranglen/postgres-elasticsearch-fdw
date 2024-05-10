e8b63e036d45eadd2bcf7395b098c5b379d0eb9d
Matthew Franglen <matthew@franglen.org>
2024-05-10 23:56:24 +0100

    Increase version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     setup(
         name="pg_es_fdw",
         packages=["pg_es_fdw"],
-        version="0.11.3",
+        version="0.12.0",
         description="Connect PostgreSQL and Elastic Search with this Foreign Data Wrapper",
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
